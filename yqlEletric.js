d7b175d113e5671782bf6ba95b3a03ea86295eb7
clj2289 <clj2289@gmail.com>
2013-06-16 18:39:23 -0400

    working stuff

--- yqlEletric.js
+++ yqlEletric.js
@@ -1,90 +1,91 @@
-var parcels = "http://tlcinter.leoncountyfl.gov/TLCAGS/rest/services/MapServices/TLC_PropertyInfo/MapServer/1/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=ADDR1+%3D+%272115+FERNLEIGH+DR%27&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=&outFields=*&f=pjson";
-parcels = "http://tlcinter.leoncountyfl.gov/TLCAGS/rest/services/MapServices/TLC_PropertyInfo/MapServer/1/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=OWNER1+like+%27%25JUDD%25%27&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=&outFields=*&f=pjson"; 
-//parcels = "http://tlcinter.leoncountyfl.gov/TLCAGS/rest/services/MapServices/TLC_PropertyInfo/MapServer/1/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=OWNER1+like+%27%25JUDD+CH%25%27&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=&outFields=*&f=pjson";
-//parcels = "http://tlcinter.leoncountyfl.gov/TLCAGS/rest/services/MapServices/TLC_PropertyInfo/MapServer/1/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=OWNER1+like+%27%25POPE+R%25%27&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=&outFields=*&f=pjson";
-//parcels = "http://tlcinter.leoncountyfl.gov/TLCAGS/rest/services/MapServices/TLC_PropertyInfo/MapServer/1/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=TAXID+%3D+%27110250+CM0020%27&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=&outFields=*&f=pjson";
-//parcels = "http://tlcinter.leoncountyfl.gov/TLCAGS/rest/services/MapServices/TLC_PropertyInfo/MapServer/1/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=TAXID+%3D+%27110250+AB0020%27&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=&outFields=*&f=pjson";
-//parcels = "http://tlcinter.leoncountyfl.gov/TLCAGS/rest/services/MapServices/TLC_PropertyInfo/MapServer/1/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=1%3D1&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=&outFields=*&f=json";
+function a() {
 
-var Client = require('node-rest-client').Client;
+    var parcels = null;
+    //parcels = "http://tlcinter.leoncountyfl.gov/TLCAGS/rest/services/MapServices/TLC_PropertyInfo/MapServer/1/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=ADDR1+%3D+%272115+FERNLEIGH+DR%27&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=&outFields=*&f=pjson";
+    //parcels = "http://tlcinter.leoncountyfl.gov/TLCAGS/rest/services/MapServices/TLC_PropertyInfo/MapServer/1/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=OWNER1+like+%27%25JUDD%25%27&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=&outFields=*&f=pjson"; 
+    //parcels = "http://tlcinter.leoncountyfl.gov/TLCAGS/rest/services/MapServices/TLC_PropertyInfo/MapServer/1/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=OWNER1+like+%27%25JUDD+CH%25%27&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=&outFields=*&f=pjson";
+    //parcels = "http://tlcinter.leoncountyfl.gov/TLCAGS/rest/services/MapServices/TLC_PropertyInfo/MapServer/1/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=OWNER1+like+%27%25POPE+R%25%27&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=&outFields=*&f=pjson";
+    //parcels = "http://tlcinter.leoncountyfl.gov/TLCAGS/rest/services/MapServices/TLC_PropertyInfo/MapServer/1/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=TAXID+%3D+%27110250+CM0020%27&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=&outFields=*&f=pjson";
+    //parcels = "http://tlcinter.leoncountyfl.gov/TLCAGS/rest/services/MapServices/TLC_PropertyInfo/MapServer/1/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=TAXID+%3D+%27110250+AB0020%27&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=&outFields=*&f=pjson";
+    parcels = "http://tlcinter.leoncountyfl.gov/TLCAGS/rest/services/MapServices/TLC_PropertyInfo/MapServer/1/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=1%3D1&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=&outFields=*&f=json";
+    var Client = require('node-rest-client').Client;
+    client = new Client();
+    var sys = require('util'),
+    rest = require('restler');
+    var addressData = [];
+    rest.get(parcels).on('complete', function(data) {
 
+        var obj = JSON.parse(data);
+        for (var i = 0; i < obj.features.length; i++) {
+            var address = encodeURIComponent(obj.features[i].attributes.ADDR1);
+            var zip = encodeURIComponent(obj.features[i].attributes.ZIP1);
+            var d = {
+                    address:address,
+                    zip:zip
+            };
+            //console.log(d);
+            addressData.push(d);
+            //console.log(addressData);
+        }
+        var fs = require('fs');
 
-client = new Client();
-var taxidToPremisesIds = [];
-// direct way
-client.get(parcels, function (data, response) {
-    // parsed response body as js object
-    var obj = JSON.parse(data);
-    //console.log(obj);
-    for (var i = 0; i < obj.features.length; i++) {
+        var outputFilename = 'my.json';
 
+        fs.writeFileSync(outputFilename, JSON.stringify(addressData, null, 4));
+    });
 
+    //console.log(addressData);
+    //return addressData;
 
-        var taxidToPremisesId = {
-        	prem_id_str: null,
-            TAXID: obj.features[i].attributes.TAXID
+}
 
-        };
+function b(){
+    var fs = require('fs');
+    var YQL = require('yql');
+    var querystring = require('querystring');
+    var file = fs.readFileSync("my.json");
+    var data = JSON.parse(file);
+    console.log("done reading file");
+    var funcs = [];
+    var prems = [];
+    for (var i = 0; i <data.length; i++) {
+        funcs[i] = (function(index) { 
+            var zip = data[index].zip;
+            var address = data[index].address;
+            return function(){
+             var  prem = new YQL.exec('select * from html where url="http://datamart.talgov.com/pls/dmart/account_search.matching_premises?zip_5_str=' + zip + '&street_name_str=' + address + '&button_sw=Lookup%20Account" and xpath="//table"', function (r) {
+                            try{
+                                var p = r.query.results.table[1].tr[1].td[0].a.href;
+                                p = p.substring(p.indexOf("?") + 1, p.length);
+                                var x = querystring.parse(p)
+                                prem_id_str = x.premise_id_str;
+                                
+                                var o = {premise_id_str:prem_id_str, zip:zip, address:address};
+                                console.log(o);
+                                var fs = require('fs');
+                                var outputFilename = 'addr.json';
+                                fs.appendFileSync(outputFilename, JSON.stringify(o, null, 4));
 
-        console.log(obj.features[i].attributes.TAXID);
-        var YQL = require("yqlp");
-        var address = encodeURIComponent(obj.features[i].attributes.ADDR1);
-        var querystring = require("querystring");
-        var prem_id_str = null;
-        console.log('http://datamart.talgov.com/pls/dmart/account_search.matching_premises?zip_5_str=' + obj.features[i].attributes.ZIP1 + '&street_name_str=' + address + '&button_sw=Lookup%20Account');
-        new YQL.execp('select * from html where url="http://datamart.talgov.com/pls/dmart/account_search.matching_premises?zip_5_str=' + obj.features[i].attributes.ZIP1 + '&street_name_str=' + address + '&button_sw=Lookup%20Account" and xpath="//table"').then(function (response) {
-            try {
-                var p = response.query.results.table[1].tr[1].td[0].a.href;
-                console.log(p.indexOf("?"));
-                p = p.substring(p.indexOf("?") + 1, p.length);
-                console.log(p);
-                var x = querystring.parse(p)
-                prem_id_str = x.premise_id_str;
-                console.log(prem_id_str);
+                                
+                            } catch (e){
+                                console.log(e);
 
-
-                taxidToPremisesId.prem_id_str = x.premise_id_str;
-               
-
-                taxidToPremisesIds.push(taxidToPremisesId);
-                //console.log(querystring.parse(p));
-
-                //for (var i = 0; i < response.query.results.length; i++) {
-                //	console.log(response.query.results[i]);
-                //}
-
-                new YQL.execp('select * from html where url="http://datamart.talgov.com/pls/dmart/dm_www_user.consumption_form.display_results?prem_id_str=' + prem_id_str + '&svc_type_str=E" and xpath="//table//table[1]/tr"').then(function (response) {
-                    console.log("jere");
-                    console.log(response);
-                    for (var i = 0; i < response.query.results.tr.length; i++) {
-                        try {
-                            console.log(response.query.results.tr[i].td[0].p);
-                            console.log(response.query.results.tr[i].td[3].p);
-                        } catch (err) {
-                            console.log(err);
-                        }
-                    };
-    					var fs = require('fs');
-
-
-					    var outputFilename = 'taxidToPremisesIds.json';
-
-					    fs.writeFileSync(outputFilename, JSON.stringify(taxidToPremisesIds));
-
-                });
-
-            } catch (err) {
-                console.log(err);
-
+                            }
+                        });
+             
             }
-                    
-
-        });
-
-
+        })(i);
 
     };
+    var premsData  = [];
+    for (var j = 0; j < data.length; j++) {
+        funcs[j]();           
+    }
+
+    
+}
 
 
+//a();
+console.log(b());
 
-});
